2c21b163f71b5eec5ef58884f529f9694b9dabf2
Imanuel Ulbricht <me@imanuel.dev>
2022-03-19 03:50:35 +0100

    Updated module

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -7,7 +7,11 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #endif
-#include "libs/libthingy/libthingy.h"
+#ifdef MINGW
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+#include <libthingy.h>
 
 void MainWindow::loadData(wxCommandEvent &event) {
     auto client = thingy::ThingiverseClient("10e4e2ef41ebdb349b72975474c5f2e0");
