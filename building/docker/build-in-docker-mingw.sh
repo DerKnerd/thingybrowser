50b6abd04b9378062c49805052ccb0799965a4a3
Imanuel Ulbricht <me@imanuel.dev>
2022-08-13 21:41:59 +0200

    Only use four cores in docker builds

--- building/docker/build-in-docker-mingw.sh
+++ building/docker/build-in-docker-mingw.sh
@@ -2,5 +2,5 @@ mkdir -p /build
 rsync -a /thingybrowser /build/
 cd /build/thingybrowser/
 cmake -DCMAKE_TOOLCHAIN_FILE=building/cmake/mingw-w64-x86_64.cmake -B /build/thingybrowser/build -S . -DCMAKE_BUILD_TYPE=Release -DMINGW=1
-cmake --build /build/thingybrowser/build --target thingybrowser -- -j12
+cmake --build /build/thingybrowser/build --target thingybrowser -- -j4
 cp /build/thingybrowser/build/thingybrowser.exe /out/
--- building/docker/build-in-docker-ubuntu.sh
+++ building/docker/build-in-docker-ubuntu.sh
@@ -2,5 +2,5 @@ mkdir -p /build
 rsync -a /thingybrowser /build/
 cd /build/thingybrowser/
 cmake -B /build/thingybrowser/build -S . -DCMAKE_BUILD_TYPE=Release
-cmake --build /build/thingybrowser/build --target thingybrowser -- -j12
+cmake --build /build/thingybrowser/build --target thingybrowser -- -j4
 cp /build/thingybrowser/build/thingybrowser /out/
